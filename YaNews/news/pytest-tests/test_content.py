9c6c654f53f6b8ed655e4ae8c06b5b75c0aef5af
dmitriilysenko <dimo_07@mail.ru>
2026-02-13 04:46:13 +0300

    add ya-news

---
+++ YaNews/news/pytest-tests/test_content.py
@@ -0,0 +1,46 @@
+from django.conf import settings
+
+import pytest
+
+from news.forms import CommentForm
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures('all_news')
+def test_news_count(client, home_url):
+    response = client.get(home_url)
+    object_list = response.context['object_list']
+    assert object_list.count() == settings.NEWS_COUNT_ON_HOME_PAGE
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures('all_news')
+def test_news_order(client, home_url):
+    response = client.get(home_url)
+    object_list = response.context['object_list']
+    all_dates = [news.date for news in object_list]
+    sorted_dates = sorted(all_dates, reverse=True)
+    assert all_dates == sorted_dates
+
+
+@pytest.mark.django_db
+def test_comments_order(client, detail_url):
+    response = client.get(detail_url)
+    assert 'news' in response.context
+    news = response.context['news']
+    all_comments = news.comment_set.all()
+    all_timestamps = [comment.created for comment in all_comments]
+    sorted_timestamps = sorted(all_timestamps)
+    assert all_timestamps == sorted_timestamps
+
+
+@pytest.mark.django_db
+def test_anonymous_client_has_no_form(client, detail_url):
+    response = client.get(detail_url)
+    assert 'form' not in response.context
+
+
+def test_authorized_client_has_form(author_client, detail_url):
+    response = author_client.get(detail_url)
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], CommentForm)
